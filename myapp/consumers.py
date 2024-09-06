cc75cbbc1c4a0517f582e07b00e98afe9119f1b1
zono0013 <zono@zonos-Mac.local>
2024-09-07 04:45:03 +0900

    add chat

---
+++ myapp/consumers.py
@@ -0,0 +1,45 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from django.contrib.auth.models import User
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = 'main'
+        self.room_group_name = f'chat_{self.room_name}'
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        user = self.scope["user"]
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'user': user.username,
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        user = event['user']
+
+        await self.send(text_data=json.dumps({
+            'type': 'chat',
+            'message': message,
+            'user': user,
+        }))
+
+    @database_sync_to_async
+    def save_message(self, user, room, message):
+        Message.objects.create(user=user, room=room, content=message)
--- myproject/asgi.py
+++ myproject/asgi.py
@@ -10,7 +10,18 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+import myapp.routing
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            myapp.routing.websocket_urlpatterns
+        )
+    ),
+})
